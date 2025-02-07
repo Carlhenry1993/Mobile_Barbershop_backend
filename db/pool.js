7c033b93bdaf303575b76513b37537764b23f248
Carlh <carlhenryfortunatinfo@gmail.com>
2025-02-07 17:31:22 -0500

    Update pool.js with SSL and IPv4 settings

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,15 @@
-const { Pool } = require('pg');
-require('dotenv').config(); // Load .env variables
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL, // This should include ?sslmode=require
+  ssl: { rejectUnauthorized: false },
+  // Force IPv4 (if supported by the underlying connection code)
+  family: 4,
 });
 
 pool.connect()
-  .then(() => console.log('Connexion réussie à PostgreSQL !'))
-  .catch((err) => console.error('Erreur de connexion à PostgreSQL :', err));
+  .then(() => console.log("Connexion réussie à PostgreSQL !"))
+  .catch((err) => console.error("Erreur de connexion à PostgreSQL :", err));
 
 module.exports = pool;
