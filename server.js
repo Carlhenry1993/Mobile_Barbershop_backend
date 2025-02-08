48aa8190f236af8f6d9acf5f6b019e87190e835f
Carlh <carlhenryfortunatinfo@gmail.com>
2025-02-08 16:05:55 -0500

    Add server.js and .gitignore

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://mobile-barbershop-frontend.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
