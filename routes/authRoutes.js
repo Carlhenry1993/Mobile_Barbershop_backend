c42c008c4291b0622df7d89a1d586732fb685685
Carlh <carlhenryfortunatinfo@gmail.com>
2025-02-08 17:45:09 -0500

    Update authRoutes.js file from repository

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,22 +6,22 @@ const router = express.Router();
 
 // Route d'inscription
 router.post(
-    '/register',
-    [
-        body('username').notEmpty().withMessage('Le nom d’utilisateur est requis.'),
-        body('password').isLength({ min: 6 }).withMessage('Le mot de passe doit contenir au moins 6 caractères.'),
-    ],
-    register // Contrôleur pour gérer l'inscription
+  '/register',
+  [
+    body('username').notEmpty().withMessage('Le nom d’utilisateur est requis.'),
+    body('password').isLength({ min: 6 }).withMessage('Le mot de passe doit contenir au moins 6 caractères.'),
+  ],
+  register // Contrôleur pour gérer l'inscription
 );
 
 // Route de connexion
 router.post(
-    '/login',
-    [
-        body('username').notEmpty().withMessage('Le nom d’utilisateur est requis.'),
-        body('password').notEmpty().withMessage('Le mot de passe est requis.'),
-    ],
-    login // Contrôleur pour gérer la connexion
+  '/login',
+  [
+    body('username').notEmpty().withMessage('Le nom d’utilisateur est requis.'),
+    body('password').notEmpty().withMessage('Le mot de passe est requis.'),
+  ],
+  login // Contrôleur pour gérer la connexion
 );
 
 module.exports = router;
