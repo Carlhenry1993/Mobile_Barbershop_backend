3d1d6e2641c3ec9c7a160a01d28ac67d7ca398a2
Carlh <carlhenryfortunatinfo@gmail.com>
2026-05-02 02:29:09 -0400

    Update db/pool.js connection for supabase migration

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,10 @@
-// db/pool.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-    "postgresql://postgres.dknknkrxlhvhouyvldvd:FAAqkazgy%401993@aws-1-us-east-1.pooler.supabase.com:6543/postgres",
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
